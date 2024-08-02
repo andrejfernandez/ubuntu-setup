225d99810e1e90a6f8e2f596c57a9e98f41d69f6
andrejfernandez <dev@andrejfernandez.io>
2024-08-01 21:21:45 -0400

    Updated setup

--- setup.sh
+++ setup.sh
@@ -1,41 +1,40 @@
 #!/bin/bash
-echo "Ubuntu Setup | github.com/andrejfernandez/ubuntu-setup\n"
+printf "Ubuntu Setup | github.com/andrejfernandez/ubuntu-setup\n"
 
 # Add repositores
-clear
-echo "Adding repositories...\n"
-sudo add-apt-repository ppa:zhangsongcui3371/fastfetch
+printf "Adding repositories...\n"
+yes | sudo add-apt-repository ppa:zhangsongcui3371/fastfetch
 
 # Updates
 clear
-echo "Updating and upgrading the system...\n"
+printf "Updating and upgrading the system...\n"
 sudo apt-get update
 sudo apt-get upgrade -y
 
 # Install base packages
 clear
-echo "Installing base packages...\n"
+printf "Installing base packages...\n"
 sudo apt install curl zsh fastfetch fzf
 
 # Install zoxide
 clear
-echo "Installing zoxide...\n"
+printf "Installing zoxide...\n"
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
 
 # Install oh-my-zsh
 clear
-echo "Installing oh-my-zsh...\n"
+printf "Installing oh-my-zsh...\n"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # Install oh-my-zsh plugins
 clear
-echo "Installing oh-my-zsh plugins...\n"
+printf "Installing oh-my-zsh plugins...\n"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Setup dotfiles
 clear
-echo "Setting up dotfiles...\n"
+printf "Setting up dotfiles...\n"
 cp -r ./dotfiles ~/
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/fastfetch/config.jsonc ~/.config/fastfetch/config.jsonc
