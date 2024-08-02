65294c2a6706aea673b2907a8b9f092a63c4279e
andrejfernandez <dev@andrejfernandez.io>
2024-08-01 21:53:32 -0400

    Updated setup

--- dotfiles/.zshrc
+++ dotfiles/.zshrc
@@ -120,10 +120,6 @@ welcome_message='Welcome home, sir.'
 typewriter_effect "$welcome_message"
 # sleep 0.5 && clear
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 # zxoide ZSH
 export PATH="$HOME/.local/bin:$PATH"
 eval "$(zoxide init zsh)"
--- setup.sh
+++ setup.sh
@@ -32,6 +32,7 @@ clear
 echo -e "Installing oh-my-zsh plugins...\n"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
 
 # Setup dotfiles
 clear
